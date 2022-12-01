1ee5dccfd38cdd5471857a1960ceea36c0b1eea7
Jacob Larte - Flutter  developer <75916231+Jacob-dvlp@users.noreply.github.com>
2022-12-01 01:27:00 +0100

    Revert "enhance scrolling behaviour"

--- lib/presetation/home/widget/body_list_widget.dart
+++ lib/presetation/home/widget/body_list_widget.dart
@@ -17,122 +17,131 @@ class BodyListWidget extends StatelessWidget {
             borderRadius: const BorderRadius.only(
                 topLeft: Radius.circular(30), topRight: Radius.circular(30)),
           ),
+          height: 400,
           padding: const EdgeInsets.all(0.0),
-          child: ListView.builder(
-            physics:  const NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: controller.model.length,
-            itemBuilder: (BuildContext context, int index) {
-              final ModeShopping model = controller.model[index];
-              return Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      height: 150,
-                      child: Row(
-                        mainAxisAlignment:
-                            MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(12.0),
-                            child: Container(
-                              child: Column(
+          child: Stack(
+            children: [
+              Positioned(
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: SizedBox(
+                  height: 450,
+                  child: ListView.builder(
+                    itemCount: controller.model.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final ModeShopping model = controller.model[index];
+                      return Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Stack(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              height: 150,
+                              child: Row(
                                 mainAxisAlignment:
-                                    MainAxisAlignment.start,
-                                crossAxisAlignment:
-                                    CrossAxisAlignment.start,
+                                    MainAxisAlignment.spaceEvenly,
                                 children: [
-                                  Text(
-                                    'New Release',
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        color:
-                                            AppColor.backgroundColor),
-                                  ),
-                                  Text(
-                                    model.name,
-                                    style: TextStyle(
-                                      fontSize: 30,
-                                      color: AppColor.backgroundColor,
-                                      fontWeight: FontWeight.w700,
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  SizedBox(
-                                    child: Text(
-                                      '\$${model.price}',
-                                      style: TextStyle(
-                                        fontSize: 14,
-                                        color: AppColor.backgroundColor,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(
-                                    height: 10,
-                                  ),
-                                  Flexible(
+                                  Padding(
+                                    padding: const EdgeInsets.all(12.0),
                                     child: Container(
-                                      decoration: BoxDecoration(
-                                        color: AppColor.backgroundColor,
-                                        borderRadius:
-                                            BorderRadius.circular(20),
-                                      ),
-                                      width: 100,
-                                      height: 30,
-                                      child: const Center(
-                                        child: Text(
-                                          'Shop now',
-                                          style: TextStyle(
-                                            color: Colors.white,
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'New Release',
+                                            style: TextStyle(
+                                                fontSize: 20,
+                                                color:
+                                                    AppColor.backgroundColor),
                                           ),
-                                        ),
+                                          Text(
+                                            model.name,
+                                            style: TextStyle(
+                                              fontSize: 30,
+                                              color: AppColor.backgroundColor,
+                                              fontWeight: FontWeight.w700,
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 10,
+                                          ),
+                                          SizedBox(
+                                            child: Text(
+                                              '\$${model.price}',
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                color: AppColor.backgroundColor,
+                                                fontWeight: FontWeight.bold,
+                                              ),
+                                            ),
+                                          ),
+                                          const SizedBox(
+                                            height: 10,
+                                          ),
+                                          Container(
+                                            decoration: BoxDecoration(
+                                              color: AppColor.backgroundColor,
+                                              borderRadius:
+                                                  BorderRadius.circular(20),
+                                            ),
+                                            width: 100,
+                                            height: 30,
+                                            child: const Center(
+                                              child: Text(
+                                                'Shop now',
+                                                style: TextStyle(
+                                                  color: Colors.white,
+                                                ),
+                                              ),
+                                            ),
+                                          )
+                                        ],
                                       ),
                                     ),
-                                  )
+                                  ),
+                                  Container(child: Image.asset(model.image)),
                                 ],
                               ),
                             ),
-                          ),
-                          Flexible(child: Image.asset(model.image)),
-                        ],
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: AppColor.backgroundColor,
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(15),
-                            bottomRight: Radius.circular(15),
-                          ),
+                            Positioned(
+                              bottom: 0,
+                              right: 0,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: AppColor.backgroundColor,
+                                  borderRadius: const BorderRadius.only(
+                                    topLeft: Radius.circular(15),
+                                    bottomRight: Radius.circular(15),
+                                  ),
+                                ),
+                                width: 40,
+                                height: 35,
+                                child: Center(
+                                  child: Text(
+                                    model.size.toString(),
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
                         ),
-                        width: 40,
-                        height: 35,
-                        child: Center(
-                          child: Text(
-                            model.size.toString(),
-                            style: const TextStyle(
-                              color: Colors.white,
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
+                      );
+                    },
+                  ),
                 ),
-              );
-            },
+              )
+            ],
           )),
     );
   }
